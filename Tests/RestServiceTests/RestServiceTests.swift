893fa7788d77b6fda32cb34a7538f74dcd521091
carlodenardin <64202858+carlodenardin@users.noreply.github.com>
2022-10-16 10:01:52 +0200

    Added the create postgres user function on the rest service (#4)

--- Tests/RestServiceTests/RestServiceTests.swift
+++ Tests/RestServiceTests/RestServiceTests.swift
@@ -1,11 +1,25 @@
-@testable import RestServiceTests
+@testable import RestService
 import XCTest
 
-final class RestServiceTests: XCTestCase {
+final class RestServiceTest: XCTestCase {
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
     func testExample() throws {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(serva().text, "Hello, World!")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
 }
